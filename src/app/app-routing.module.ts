165a88675b209f35e2af4e94424306e2d7dc13c0
georg_un <onkovic@gmail.com>
2019-04-28 12:10:53 +0200

    Add routing & implement transaction-list & overview

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TransactionsComponent} from "./transactions/transactions.component";
+import {OverviewComponent} from "./overview/overview.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'transactions',
+    component: TransactionsComponent
+  },
+  {
+    path: 'overview',
+    component: OverviewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
