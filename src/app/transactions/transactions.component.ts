ad055f09380340102b3be78d557ce4be52935e44
georg_un <onkovic@gmail.com>
2019-04-28 12:09:13 +0200

    Add transaction-list component

---
+++ src/app/transactions/transactions.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {MockRestService} from "../rest-service/mock-rest.service";
+import {Transaction} from "../rest-service/entity/transaction";
+
+@Component({
+  selector: 'app-transactions',
+  templateUrl: './transactions.component.html',
+  styleUrls: ['./transactions.component.scss']
+})
+export class TransactionsComponent implements OnInit {
+
+  transactions: Transaction[];
+
+  constructor(private mockRestService: MockRestService) {
+    this.transactions = this.mockRestService.fetchTransactions();
+  }
+
+  ngOnInit() {
+  }
+
+}
