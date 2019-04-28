753d5571bbf29a8e41e400e469875db7b32312d1
georg_un <onkovic@gmail.com>
2019-04-28 12:07:35 +0200

    Introduce header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {SidenavService} from "../sidenav/sidenav.service";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor(private sidenavService: SidenavService) { }
+
+  ngOnInit() {
+  }
+
+  toggleSidenav(): void {
+    this.sidenavService.toogle();
+  }
+
+}
