520cebb27463019d26c882bc6af09db42901b6dc
georg_un <onkovic@gmail.com>
2019-07-11 21:11:50 +0200

    Introduce payment editor component

---
+++ src/app/payment-editor/payment-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-editor',
+  templateUrl: './payment-editor.component.html',
+  styleUrls: ['./payment-editor.component.scss']
+})
+export class PaymentEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
