d929a631f625beab726043683dba843aa7cb1020
georg_un <onkovic@gmail.com>
2019-10-19 15:31:52 +0200

    Minor changes

--- src/app/core/rest-service/mock-data/users.ts
+++ src/app/core/rest-service/mock-data/users.ts
@@ -2,24 +2,24 @@ import { User } from '../entity/user';
 
 export const USERS: User[] = [
   {
-    'userId': 1,
-    'username': 'alice',
-    'firstName': 'Alice',
-    'lastName': 'Cooper',
-    'sumDebitAmounts': -13.469999999999999
+    userId: 1,
+    username: 'alice',
+    firstName: 'Alice',
+    lastName: 'Cooper',
+    sumDebitAmounts: -13.469999999999999
   },
   {
-    'userId': 2,
-    'username': 'bob',
-    'firstName': 'Bob',
-    'lastName': 'Ross',
-    'sumDebitAmounts': 43.589999999999996
+    userId: 2,
+    username: 'bob',
+    firstName: 'Bob',
+    lastName: 'Ross',
+    sumDebitAmounts: 43.589999999999996
   },
   {
-    'userId': 3,
-    'username': 'charlie',
-    'firstName': 'Charlie',
-    'lastName': 'Brown',
-    'sumDebitAmounts': -30.119999999999997
+    userId: 3,
+    username: 'charlie',
+    firstName: 'Charlie',
+    lastName: 'Brown',
+    sumDebitAmounts: -30.119999999999997,
   }
 ]
