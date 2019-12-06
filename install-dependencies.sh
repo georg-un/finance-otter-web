7564052e7c772f8349772aa189cceed564adb617
georg_un <onkovic@gmail.com>
2019-12-06 10:41:36 +0100

    [Dist] Upgrade Angular verison, TS version, and other dependencies

---
+++ install-dependencies.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Note: You may have to upgrade your angular version with:
+# sudo npm install -g @angular/cli
+# Subsequently, you will have to update the projects' dependencies with:
+# ng update @angular/cli @angular/core
+
+# Install ngrx packages
+npm install @ngrx/store --save
+npm install @ngrx/store-devtools --save
+npm install @ngrx/effects --save
+npm install @ngrx/router-store --save
+npm install @ngrx/entity --save
