61af66e5be32823ee012b7cbf4bed59939ccfaf3
georg-un <georg.unterholzner.coding@gmail.com>
2020-01-04 13:28:42 +0100

    [Style] Add route animation for opening/closing payment detail view

--- src/app/layout/layout.animations.ts
+++ src/app/layout/layout.animations.ts
@@ -72,7 +72,6 @@ export const expandFromFAB =
         })])
       ]),
       query(':leave', animateChild()),
-
       group([
         // Expand the FAB over the whole view, switch the background-color and increase the opacity
         query('.fab-button', [
@@ -141,7 +140,6 @@ export const expandFromFAB =
         })])
       ]),
       query(':leave', animateChild()),
-
       group([
         // Expand the FAB over the whole view, switch the background-color and increase the opacity
         query('.fab-button', [
@@ -154,7 +152,6 @@ export const expandFromFAB =
             backgroundColor: '#e53935'
           }))
         ]),
-
         query(':leave', [
           animate('250ms ease-in', style({
             overflowY: 'hidden',
@@ -169,6 +166,105 @@ export const expandFromFAB =
         ]),
       ]),
       query(':enter', animateChild()),
-    ])
+    ]),
+
+
+    // Expand detail view from bottom
+    transition('Overview => PaymentView', [
+      // Set up views
+      group([
+        // Keep the leave view fixed
+        query(':leave', [style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          paddingTop: 'var(--header-height)'
+        })],{
+          limit: 1
+        }),
+        // Place the enter view at the bottom with a height of 0
+        query(':enter', [style({
+          overflowY: 'hidden',
+          position: 'absolute',
+          bottom: 0,
+          right: 0,
+          height: 0,
+          width: '100%',
+          paddingTop: 'var(--header-height)',
+          zIndex: 2
+        })]),
+        // Set the height of the payment view detail container to 100%
+        query('.detail-container', [style({
+          marginTop: 0,
+          height: '100%'
+        })])
+      ]),
+      query(':leave', animateChild()),
+      // Expand the enter view from the bottom
+      query(':enter', [
+        animate('150ms ease-out', style({
+          height: 'calc(100vh - var(--header-height))',
+        }))
+      ]),
+      // Move the detail container down to reveal the payment detail header
+      query('.detail-container', [
+        animate('200ms ease-out', style({
+          marginTop: 'calc(var(--header-height)/2 + var(--payment-header-height))'
+        }))
+      ]),
+      query(':enter', animateChild()),
+    ]),
+
+
+    // Collapse detail view into bottom
+    transition('PaymentView => Overview', [
+      // Set up views
+      group([
+        // Keep the leave view fixed
+        query(':enter', [style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          paddingTop: 'var(--header-height)'
+        })],{
+          limit: 1
+        }),
+        // Place the enter view at the bottom with a height of 0
+        query(':leave', [style({
+          overflowY: 'hidden',
+          position: 'absolute',
+          bottom: 0,
+          right: 0,
+          width: '100%',
+          height: 'calc(100vh - var(--header-height))',
+          // z-depth 2 reversed:
+          '-webkit-box-shadow': '0 -4px 5px 0 rgba(0, 0, 0, 0.14), 0 -1px 10px 0 rgba(0, 0, 0, 0.12)',
+          'box-shadow': '0 -4px 5px 0 rgba(0, 0, 0, 0.14), 0 -1px 10px 0 rgba(0, 0, 0, 0.12)'
+        })],{
+          limit: 1
+        }),
+        // Set height of detail container to 100% for later
+        query('.detail-container', [ style({
+            height: '100%'
+          })
+        ]),
+      ]),
+      query(':leave', animateChild()),
+      // Expand the detail container over the whole view
+      query('.detail-container', [
+        animate('150ms ease-out', style({
+          marginTop: '0'
+        }))
+      ]),
+      // Collapse the leave view into the bottom
+      query(':leave', [
+        animate('150ms ease-in', style({
+          height: 0
+        }))
+      ]),
+      query(':enter', animateChild()),
+    ]),
 
   ]);
