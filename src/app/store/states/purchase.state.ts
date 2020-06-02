4916c029c270f09a201f3900c995706e8fb41e3f
georg-un <georg.unterholzner.coding@gmail.com>
2020-06-02 21:07:50 +0200

    [Fix] Fix order of newly added purchases

--- src/app/store/states/purchase.state.ts
+++ src/app/store/states/purchase.state.ts
@@ -5,19 +5,20 @@ export interface PurchaseState extends EntityState<Purchase> {
   syncJobs: number;
 }
 
-export const purchaseAdapter: EntityAdapter<Purchase> = createEntityAdapter<Purchase>({
-  selectId: (purchase: Purchase) => purchase.purchaseId
-});
-
 export function sortByDate(a: Purchase, b: Purchase): number {
   if (a.date > b.date) {
-    return 1;
-  } else if (a.date < b.date) {
     return -1;
+  } else if (a.date < b.date) {
+    return 1;
   } else {
     return 0;
   }
 }
+
+export const purchaseAdapter: EntityAdapter<Purchase> = createEntityAdapter<Purchase>({
+  selectId: (purchase: Purchase) => purchase.purchaseId,
+  sortComparer: sortByDate
+});
 
 export const initialState: PurchaseState = purchaseAdapter.getInitialState({
   syncJobs: 0,
