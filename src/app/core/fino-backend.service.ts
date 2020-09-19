1653771d05416b2b1d811e4d0e55dfbe353a463f
georg-un <georg.unterholzner.coding@gmail.com>
2020-09-19 21:33:23 +0200

    [Fix] Retry on http errors

--- src/app/core/fino-backend.service.ts
+++ src/app/core/fino-backend.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { FinOBackendServiceInterface } from './fino-backend.service.interface';
-import { Observable, throwError } from 'rxjs';
+import { MonoTypeOperatorFunction, Observable, throwError } from 'rxjs';
 import { Purchase } from './entity/purchase';
 import { environment } from '../../environments/environment';
 import { User } from './entity/user';
-import { catchError } from 'rxjs/operators';
+import { catchError, retry } from 'rxjs/operators';
 import { MatSnackBar } from '@angular/material';
 import { MultilineSnackbarComponent } from '../shared/multiline-snackbar/multiline-snackbar.component';
 import { ChartSeries } from './entity/chart-series';
@@ -32,13 +32,13 @@ export class FinOBackendService implements FinOBackendServiceInterface {
 
   checkIfUserActive(): Observable<boolean> {
     return this.http.get<boolean>(this.endpoints.users + '/current').pipe(
-      catchError((err) => this.handleError(err))
+      this.handleRequestFailure()
     );
   }
 
   createNewUser(user: User): Observable<User> {
     return this.http.post<User>(this.endpoints.users, user).pipe(
-      catchError((err) => this.handleError(err))
+      this.handleRequestFailure()
     );
   }
 
@@ -47,25 +47,25 @@ export class FinOBackendService implements FinOBackendServiceInterface {
     params = params.set('offset', offset.toString());
     params = params.set('limit', limit.toString());
     return this.http.get<Purchase[]>(this.endpoints.purchases, {params: params}).pipe(
-      catchError((err) => this.handleError(err))
+      this.handleRequestFailure()
     );
   }
 
   fetchPurchase(purchaseId: string): Observable<Purchase> {
     return this.http.get<Purchase>(this.endpoints.purchases + `/${purchaseId}`).pipe(
-      catchError((err) => this.handleError(err))
+      this.handleRequestFailure()
     );
   }
 
   fetchUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.endpoints.users).pipe(
-      catchError((err) => this.handleError(err))
+      this.handleRequestFailure()
     );
   }
 
   fetchCategories(): Observable<Category[]> {
     return this.http.get<Category[]>(this.endpoints.categories).pipe(
-      catchError((err) => this.handleError(err))
+      this.handleRequestFailure()
     );
   }
 
@@ -73,7 +73,7 @@ export class FinOBackendService implements FinOBackendServiceInterface {
     return this.http.get(
       `${this.endpoints.purchases}/${purchaseId}/receipt`,
       {responseType: 'blob'}).pipe(
-      catchError((err => this.handleError(err)))
+      this.handleRequestFailure()
     );
   }
 
@@ -86,19 +86,19 @@ export class FinOBackendService implements FinOBackendServiceInterface {
     );
     uploadForm.append('receipt', receipt);
     return this.http.post<Purchase>(this.endpoints.purchases, uploadForm).pipe(
-      catchError((err) => this.handleError(err))
+      this.handleRequestFailure()
     );
   }
 
   updatePurchase(purchase: Purchase): Observable<Purchase> {
     return this.http.put<Purchase>(this.endpoints.purchases, purchase).pipe(
-      catchError((err) => this.handleError(err))
+      this.handleRequestFailure()
     );
   }
 
   deletePurchase(purchaseId: string): Observable<Object> {
     return this.http.delete(`${this.endpoints.purchases}/${purchaseId}`).pipe(
-      catchError((err) => this.handleError(err))
+      this.handleRequestFailure()
     );
   }
 
@@ -106,19 +106,19 @@ export class FinOBackendService implements FinOBackendServiceInterface {
     const uploadForm = new FormData();
     uploadForm.append('receipt', receipt);
     return this.http.put(`${this.endpoints.purchases}/${purchaseId}/receipt`, uploadForm).pipe(
-      catchError((err) => this.handleError(err))
+      this.handleRequestFailure()
     );
   }
 
   deleteReceipt(purchaseId: string): Observable<Object> {
     return this.http.delete(`${this.endpoints.purchases}/${purchaseId}/receipt`).pipe(
-      catchError((err) => this.handleError(err))
+      this.handleRequestFailure()
     );
   }
 
   fetchBalances(): Observable<Object> {
     return this.http.get<Object>(this.endpoints.summary + '/balance').pipe(
-      catchError((err) => this.handleError(err))
+      this.handleRequestFailure()
     );
   }
 
@@ -126,7 +126,7 @@ export class FinOBackendService implements FinOBackendServiceInterface {
     let params = new HttpParams();
     params = params.set('offset', months.toString());
     return this.http.get<ChartSeries[]>(this.endpoints.summary + '/month_category', {params: params}).pipe(
-      catchError((err) => this.handleError(err))
+      this.handleRequestFailure()
     );
   }
 
@@ -134,8 +134,15 @@ export class FinOBackendService implements FinOBackendServiceInterface {
     let params = new HttpParams();
     params = params.set('offset', months.toString());
     return this.http.get<ChartData[]>(this.endpoints.summary + '/category', {params: params}).pipe(
-      catchError((err) => this.handleError(err))
+      this.handleRequestFailure()
     );
+  }
+
+  private handleRequestFailure<T>(): MonoTypeOperatorFunction<T> {
+    return (source: Observable<T>) => source.pipe(
+      retry(3),
+      catchError(err => this.handleError(err))
+    )
   }
 
   private handleError(error: HttpErrorResponse) {
