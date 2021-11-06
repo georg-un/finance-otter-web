cf52d0dd316bfb30d4bbd8dd87c8234c08f845bc
Georg Unterholzner <georg.unterholzner.coding@gmail.com>
2021-11-06 16:13:48 +0100

    Use default grey for all buttons & minor style fixes

--- src/app/layout/layout.animations.ts
+++ src/app/layout/layout.animations.ts
@@ -3,7 +3,7 @@ import { animate, style, transition, trigger } from '@angular/animations';
 
 export const fadeOnChange = trigger('fadeOnChange', [
   transition(':leave', [
-    animate('200ms',
+    animate('100ms',
       style({
         opacity: 0,
         height: 'calc(var(--header-height) * 0.8)',
@@ -17,7 +17,7 @@ export const fadeOnChange = trigger('fadeOnChange', [
       height: 'calc(var(--header-height) * 0.8)',
       width: 'calc(var(--header-height) * 0.8)'
     }),
-    animate('200ms',
+    animate('150ms',
       style({
         opacity: 1,
         height: 'var(--header-height)',
