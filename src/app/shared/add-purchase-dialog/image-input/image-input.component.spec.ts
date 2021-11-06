b83e2041e42e9b46165185108846769fd7135325
Georg Unterholzner <georg.unterholzner.coding@gmail.com>
2021-11-06 23:30:27 +0100

    Add unit tests for image input component

--- src/app/shared/add-purchase-dialog/image-input/image-input.component.spec.ts
+++ src/app/shared/add-purchase-dialog/image-input/image-input.component.spec.ts
@@ -1,6 +1,7 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 
-import { ImageInputComponent } from './image-input.component';
+import { HTMLInputEvent, ImageInputComponent } from './image-input.component';
+import { By } from '@angular/platform-browser';
 
 describe('ImageInputComponent', () => {
   let component: ImageInputComponent;
@@ -21,5 +22,33 @@ describe('ImageInputComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should trigger a camera input', () => {
+    const inputRef = fixture.debugElement.query(By.css('#cameraInput'));
+    let hasBeenClicked = false;
+    inputRef.nativeElement.click = () => hasBeenClicked = true;
+    component.triggerCameraInput();
+    expect(hasBeenClicked).toEqual(true);
+  });
+
+  it('should trigger a file input', () => {
+    const inputRef = fixture.debugElement.query(By.css('#fileInput'));
+    let hasBeenClicked = false;
+    inputRef.nativeElement.click = () => hasBeenClicked = true;
+    component.triggerFileInput();
+    expect(hasBeenClicked).toEqual(true);
+  });
+
+  it('should emit an imageInput event iff the input is valid', (done: DoneFn) => {
+    const validImageInput = {target: {files: {0: {}}}} as unknown as HTMLInputEvent;
+    component.imageInput.subscribe(event => {
+      expect(event).toEqual(validImageInput.target.files);
+      done();
+    });
+    component.onImageCapture(null);
+    component.onImageCapture({target: null} as any);
+    component.onImageCapture({target: {files: null}} as any);
+    component.onImageCapture(validImageInput);
   });
 });
