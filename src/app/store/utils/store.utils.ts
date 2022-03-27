7d04bb6918694dd4f2eaf805ae2bd35bb3067278
Georg Unterholzner <georg.unterholzner.coding@gmail.com>
2022-03-27 18:30:54 +0200

    Implement summary state with NGXS

--- src/app/store/utils/store.utils.ts
+++ src/app/store/utils/store.utils.ts
@@ -4,3 +4,9 @@ import cloneDeep from 'lodash-es'
 export function getClonedState<T>(ctx: StateContext<T>): T {
   return cloneDeep(ctx.getState());
 }
+
+export function updateSingleStateProperty<T>(ctx: StateContext<T>, property: keyof T, value: T[keyof T]): T {
+  const newState = getClonedState(ctx);
+  newState[property] = value;
+  return ctx.setState(newState);
+}
