0241c57182fbc31bb66aea733b5311d7dd55b7c7
Georg Unterholzner <georg.unterholzner.coding@gmail.com>
2022-03-29 00:12:19 +0200

    Add typescript path mapping for store

---
+++ src/app/store/_index.ts
@@ -0,0 +1,13 @@
+export * as CategoryActions from './category/category.actions';
+export { CategoryState } from './category/category.state';
+
+export * as PurchaseActions from './purchase/purchase.actions';
+export { PurchaseState } from './purchase/purchase.state';
+
+export * as SummaryActions from './summary/summary.actions';
+export { SummaryState } from './summary/summary.state';
+
+export * as UserActions from './user/user.actions';
+export { UserState } from './user/user.state';
+
+export { AppStoreModule } from './app-store.module';
