9036cb706b632ea65edc63688eb091a7261cd54c
Georg Unterholzner <georg.unterholzner.coding@gmail.com>
2022-04-18 22:51:37 +0200

    Add @ngxs/devtools-plugin dependency

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {NgxsModule} from '@ngxs/store';
+import {NgxsReduxDevtoolsPluginModule} from '@ngxs/devtools-plugin';
 import {CategoryState} from './category/category.state';
 import {PurchaseState} from './purchase/purchase.state';
 import {SummaryState} from './summary/summary.state';
 import {UserState} from './user/user.state';
+import {environment} from '../../environments/environment';
 
 
 @NgModule({
@@ -15,7 +17,11 @@ import {UserState} from './user/user.state';
       PurchaseState,
       SummaryState,
       UserState
-    ])
+    ]),
+    NgxsReduxDevtoolsPluginModule.forRoot({
+      disabled: environment.production
+    })
   ]
 })
-export class StoreModule { }
+export class StoreModule {
+}
